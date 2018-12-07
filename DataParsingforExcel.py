8b77ba6349a31160154bc7a847283ea0f0aa4e75
Todd Koym <todd.koym@apexcleanenergy.com>
2018-12-07 13:12:30 -0500

    Follow PEP8 style

--- DataParsingforExcel.py
+++ DataParsingforExcel.py
@@ -1,18 +1,19 @@
 import CFARS_SS_Phase1_Analysis as m
-from openpyxl.utils.dataframe import dataframe_to_rows
+# from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl import Workbook
 
 
 def writedatainbins(inputdata, results_filename):
     wb = Workbook()
-    
-    for i in range(2,21):
+
+    for i in range(2, 21):
         sheetname = '{}mps_bin'.format(i)
         wb.create_sheet(sheetname)
         ws = wb[sheetname]
-        df = inputdata[inputdata['bins']==i]
-        m.write_resultstofile(df,ws, 1,1)
+        df = inputdata[inputdata['bins'] == i]
+        m.write_resultstofile(df, ws, 1, 1)
     wb.save(results_filename)
+
 
 if __name__ == '__main__':
     input_filename, config_file, results_filename = m.get_inputfiles()
